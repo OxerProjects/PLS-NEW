8c3575082db2074fd84cdd27c282c3c01f4547d9
OxerProjects <oxerprojects@gmail.com>
2024-11-30 14:14:23 +0200

    PLS 30/11/24 2.1

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
-const Course = require('../models/Course');
+const Course = require('../models/Cours');
 const Lessons = require('../models/Lesson');
 const Quizs = require('../models/Quiz');
 
